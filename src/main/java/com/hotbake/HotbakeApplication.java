ffdb1b619252c5de37f8220e3d8f882ef69b4063
dipra <dipra5565@gmail.com>
2026-03-23 16:47:15 +0600

    Project initialized without docker setup.

---
+++ src/main/java/com/hotbake/HotbakeApplication.java
@@ -0,0 +1,13 @@
+package com.hotbake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotbakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotbakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotbake/HotbakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotbake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotbakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
