1e5aac1d9e1eacaf6fea70a428aa189e3cdf2bda
rahman-arifur <arifurr946@gmail.com>
2026-04-04 17:13:09 +0600

    Final Touch with the products and fragments and some finalized files

---
+++ src/main/java/com/hotbake/HotBakeApplication.java
@@ -0,0 +1,11 @@
+package com.hotbake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotBakeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotBakeApplication.class, args);
+    }
+}
